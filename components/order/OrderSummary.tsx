f5bbaedd9ba5ba048be800f92f662cd10678e0b6
ivan <ivanjv1234@gmail.com>
2025-06-21 00:16:42 -0600

    feat: main components

---
+++ components/order/OrderSummary.tsx
@@ -0,0 +1,10 @@
+
+const OrderSummary = () => {
+  return (
+    <div className="md:w-64 lg:w-96 p-5 lg:h-screen lg:overflow-y-scroll">
+      <h1 className="text-4xl font-black text-center">Mi pedido</h1>
+    </div>
+  )
+}
+
+export default OrderSummary
