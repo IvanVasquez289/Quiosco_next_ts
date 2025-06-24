0a516c9afdff5932bfc6952f75417cf0b190d3f5
ivan <ivanjv1234@gmail.com>
2025-06-23 18:43:02 -0600

    feat: order summary component

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,8 +1,19 @@
+"use client"
+import { useStore } from "@/src/store"
+import ProductDetails from "./ProductDetails"
 
 const OrderSummary = () => {
+  const order = useStore((state) => state.order)
   return (
     <div className="md:w-64 lg:w-96 p-5 lg:h-screen lg:overflow-y-scroll">
       <h1 className="text-4xl font-black text-center">Mi pedido</h1>
+      {order.length === 0 ? <p className="my-10 text-center">La orden esta vacia</p> : (
+        <div className="mt-5">
+          {order.map(product => (
+            <ProductDetails key={product.id} item={product}/>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/store.ts
@@ -0,0 +1,69 @@
+import { create } from "zustand";
+import { OrderItem } from "./types";
+import { Product } from "@prisma/client";
+
+interface Store {
+    order: OrderItem[],
+    addToOrder: (product: Product) => void,
+    incrementQuantity: (id: number) => void,
+    decrementQuantity: (id: number) => void
+}
+
+const MIN_QUANTITY = 1;
+const MAX_QUANTITY = 5;
+export const useStore = create<Store>((set,get)=>({
+    order: [],
+    addToOrder: (product: Product) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const {categoryId, image, ...data} = product;
+        const productExists = get().order.some(item => item.id === product.id)
+        let updatedOrder: OrderItem[] = [];
+        if(productExists){
+            updatedOrder = get().order.map(itm => {
+                if(itm.id === product.id && itm.quantity < MAX_QUANTITY){
+                    return {
+                        ...itm,
+                        quantity: itm.quantity + 1,
+                        subtotal: (itm.quantity + 1) * product.price
+                    }
+                }
+                return itm
+            })
+        }else {
+            updatedOrder = [...get().order, {
+                ...data,
+                quantity: 1,
+                subtotal: product.price
+            }]
+        }
+
+        set({order: updatedOrder})
+        
+    },
+    incrementQuantity: (id: number) => {
+        const updateOrder = get().order.map(itm => {
+            if(itm.id === id && itm.quantity < MAX_QUANTITY){
+                return {
+                    ...itm,
+                    quantity: itm.quantity + 1,
+                    subtotal: (itm.quantity + 1) * itm.price
+                }
+            }
+            return itm
+        })
+        set({order: updateOrder})
+    },
+    decrementQuantity: (id: number) => {
+        const updateOrder = get().order.map(itm => {
+            if(itm.id === id && itm.quantity > MIN_QUANTITY){
+                return {
+                    ...itm,
+                    quantity: itm.quantity - 1,
+                    subtotal: (itm.quantity - 1) * itm.price
+                }
+            }
+            return itm
+        })
+        set({order: updateOrder})
+    }
+}))
