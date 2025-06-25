547ef9a6057bd6d3775a5b5c26525624da638dc9
ivan <ivanjv1234@gmail.com>
2025-06-24 19:22:26 -0600

    feat: show order content

--- app/admin/order/page.tsx
+++ app/admin/order/page.tsx
@@ -1,10 +1,38 @@
+import OrderCard from '@/components/order/OrderCard'
 import Heading from '@/components/ui/Heading'
+import { prisma } from '@/src/lib/prisma'
 import React from 'react'
 
-const OrdersPage = () => {
+const getPendingOrders = async () => {
+  const order = await prisma.order.findMany({
+    where: {
+      status: false
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true
+        }
+      }
+    }
+  })
+
+  return order
+}
+const OrdersPage = async () => {
+  const order = await getPendingOrders()
   return (
     <>
       <Heading>Administrar Ordenes</Heading>
+      {order.length ? (
+        <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5'>
+          {order.map(order => (
+            <OrderCard key={order.id} order={order}/>
+          ))}
+        </div>
+      ) : (
+        <p className="text-center">No hay ordenes pendientes</p>
+      )}
     </>
   )
 }
