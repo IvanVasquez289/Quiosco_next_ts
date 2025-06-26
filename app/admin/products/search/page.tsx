9bc964de91f873f243d6029ecb28d089bbba1d42
ivan <ivanjv1234@gmail.com>
2025-06-25 21:53:02 -0600

    feat: search products

---
+++ app/admin/products/search/page.tsx
@@ -0,0 +1,38 @@
+import ProductSearchForm from '@/components/products/ProductSearchForm'
+import ProductTable from '@/components/products/ProductTable'
+import Heading from '@/components/ui/Heading'
+import { prisma } from '@/src/lib/prisma'
+import React from 'react'
+
+
+const searchProduct = async (searchTerm: string) => {
+    const products = await prisma.product.findMany({
+        where: {
+            name: {
+                contains: searchTerm,
+                mode: "insensitive"
+            }
+        },
+        include: {
+            category: true
+        }
+    })
+
+    return products
+}
+
+const SearchPage = async ({searchParams}: {searchParams: {search: string}}) => {
+  const {search} = await searchParams
+  const products = await searchProduct(search)
+  return (
+    <>
+        <Heading>Resultados de la busqueda: {search}</Heading>
+        <div className="flex flex-col  md:flex-row md:items-center md:justify-end gap-5">
+        <ProductSearchForm/>
+      </div>
+        <ProductTable products={products}/>
+    </>
+  )
+}
+
+export default SearchPage
