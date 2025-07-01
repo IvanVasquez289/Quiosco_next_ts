ae80345d51f7fd1a87cff8b57ec3db8203706996
ivan <ivanjv1234@gmail.com>
2025-06-30 21:52:52 -0600

    feat: update product

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,3 +1,7 @@
+import EditProductForm from '@/components/products/EditProductForm'
+import ProductForm from '@/components/products/ProductForm'
+import GoBackButton from '@/components/ui/GoBackButton'
+import Heading from '@/components/ui/Heading'
 import { prisma } from '@/src/lib/prisma'
 import { notFound } from 'next/navigation'
 
@@ -17,9 +21,14 @@ const getProductById = async (id: number) => {
 const EditPage = async ({params}: {params: {id: string}}) => {
   const {id} = await params
   const product = await getProductById(+id)
-  console.log(product)
   return (
-    <div>EditPage</div>
+    <>
+      <Heading>Editar Producto: {product.name}</Heading>
+      <GoBackButton/>
+      <EditProductForm>
+        <ProductForm product={product}/>
+      </EditProductForm>
+    </>
   )
 }
 
--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,52 +1,62 @@
 "use client";
+import { getImagePath } from "@/src/utils";
 import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 import { useState } from "react";
 import { TbPhotoPlus } from "react-icons/tb";
-const ImageUpload = () => {
+const ImageUpload = ({ image }: { image: string | undefined }) => {
   const [imageUrl, setImageUrl] = useState("");
   return (
-    <CldUploadWidget 
-        onSuccess={(result, {widget})=>{
-          if(result.event === "success"){
-            widget.close()
-            // @ts-expect-error: property 'secure_url' does exist on 'info' property
-            setImageUrl(result.info?.secure_url)
-          }
-        }}
-        uploadPreset="quioscoapp"
-        options={{
-            maxFiles: 1,
-        }}
+    <CldUploadWidget
+      onSuccess={(result, { widget }) => {
+        if (result.event === "success") {
+          widget.close();
+          // @ts-expect-error: property 'secure_url' does exist on 'info' property
+          setImageUrl(result.info?.secure_url);
+        }
+      }}
+      uploadPreset="quioscoapp"
+      options={{
+        maxFiles: 1,
+      }}
     >
       {({ open }) => (
         <>
           <div className="space-y-2">
             <label className="text-slate-800">Imagen Producto</label>
-            <div 
-                className="relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex 
+            <div
+              className="relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex 
                 flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100"
-                onClick={() => open()}
+              onClick={() => open()}
             >
               <TbPhotoPlus size={50} />
               <p className="text-lg font-semibold">Sube una imagen</p>
               {imageUrl && (
                 <div className="absolute inset-0 w-full h-full">
-                    <Image
-                        src={imageUrl}
-                        alt="Imagen de producto"
-                        fill
-                        style={{ objectFit: "contain" }}
-                    />
+                  <Image
+                    src={imageUrl}
+                    alt="Imagen de producto"
+                    fill
+                    style={{ objectFit: "contain" }}
+                  />
                 </div>
               )}
             </div>
           </div>
-          <input 
-            type="hidden" 
-            name="image"
-            value={imageUrl} 
-          />
+            {image && !imageUrl && (
+            <div className="space-y-2">
+              <label className="text-slate-800">Imagen Actual</label>
+              <div className="relative w-64 h-64 mx-auto cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100">
+                <Image
+                  src={getImagePath(image)}
+                  alt="Imagen de producto"
+                  fill
+                  style={{ objectFit: "contain" }}
+                />
+              </div>
+            </div>
+          )}
+          <input type="hidden" name="image" defaultValue={imageUrl ? imageUrl : image} />
         </>
       )}
     </CldUploadWidget>
