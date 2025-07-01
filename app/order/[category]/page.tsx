24ea60542cc8e84fc1a9da8737699e52a88a6ba9
ivan <ivanjv1234@gmail.com>
2025-07-01 00:39:30 -0600

    feat: admin panel button

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,6 +1,7 @@
 import ProductCard from "@/components/products/ProductCard"
 import Heading from "@/components/ui/Heading"
 import { prisma } from "@/src/lib/prisma"
+import Link from "next/link"
 
 
 const getProducts = async (category: string) => {
@@ -19,9 +20,18 @@ const OrderPage = async ({params}: {params: Promise<{category: string}>}) => {
   const products = await getProducts(category)
   return (
     <>
-      <Heading>
-        Elige y personaliza tu pedido
-      </Heading>
+      <div className="flex  flex-col md:flex-row gap-3 justify-between items-center mb-10 sm:mb-0">
+        <Heading>
+          Elige y personaliza tu pedido
+        </Heading>
+        <Link 
+          href={"/admin/products"}
+          className="bg-amber-400 text-black py-3 px-10 text-center font-bold cursor-pointer"
+        >
+          Admin Panel
+        </Link>
+      </div>
+      
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4  items-start">
         {products.map(product => (
           <ProductCard key={product.id} product={product}/>
--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,22 +1,21 @@
-import { prisma } from "@/src/lib/prisma"
-import CategoryIcon from "../ui/CategoryIcon"
-import Logo from "../ui/Logo"
+import { prisma } from "@/src/lib/prisma";
+import CategoryIcon from "../ui/CategoryIcon";
+import Logo from "../ui/Logo";
 
 const getCategories = async () => {
-  return await prisma.category.findMany()
-}
-const OrderSidebar =  async () => {
-  const categories = await getCategories()
+  return await prisma.category.findMany();
+};
+const OrderSidebar = async () => {
+  const categories = await getCategories();
   return (
     <aside className="md:w-72 md:h-screen bg-white">
-        <Logo />
-        <nav className="mt-10">
-          {categories.map(category => (
-            <CategoryIcon key={category.id} category={category}/>
-          ))}
-        </nav>
+      <Logo />
+      <nav className="mt-10">
+        {categories.map((category) => (
+          <CategoryIcon key={category.id} category={category} />
+        ))}
+      </nav>
     </aside>
-  )
-}
-
-export default OrderSidebar
+  );
+};
+export default OrderSidebar;
